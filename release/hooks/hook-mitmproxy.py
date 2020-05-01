6a02c819c41fa1745e5644cb8c0e5fe31d18cdce
Maximilian Hils <git@maximilianhils.com>
2020-05-01 13:20:32 +0200

    fix pkg_resources hook

--- release/hooks/hook-mitmproxy.py
+++ release/hooks/hook-mitmproxy.py
@@ -1,1 +1,5 @@
 hiddenimports = ["mitmproxy.script"]
+
+# taken from https://github.com/pyinstaller/pyinstaller/blob/develop/PyInstaller/hooks/hook-pkg_resources.py
+# at 2020-05-01, required for PyInstaller < 3.7
+hiddenimports.append('pkg_resources.py2_warn')
