154a3e49af4e581f85e7df8fa50b3d501b855733
Maximilian Hils <git@maximilianhils.com>
2020-05-08 22:35:00 +0200

    pyinstaller: add configparser hidden import

--- release/hooks/hook-mitmproxy.py
+++ release/hooks/hook-mitmproxy.py
@@ -3,3 +3,7 @@ hiddenimports = ["mitmproxy.script"]
 # taken from https://github.com/pyinstaller/pyinstaller/blob/develop/PyInstaller/hooks/hook-pkg_resources.py
 # at 2020-05-01, required for PyInstaller < 3.7
 hiddenimports.append('pkg_resources.py2_warn')
+
+# passlib depends on configparser, but the builtin passlib hook doesn't include it.
+# Since PyInstaller 3.6 we cannot have a hook with the same name, so it lives here.
+hiddenimports.append("configparser")
