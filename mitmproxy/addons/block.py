d92db77b4cb3822633d606f611809a37d8e346d0
Maximilian Hils <git@maximilianhils.com>
2020-11-17 11:56:45 +0100

    [sans-io] implement client/server (dis)connect events
    semantic are slightly changed, so we change names as well.

--- mitmproxy/addons/block.py
+++ mitmproxy/addons/block.py
@@ -20,6 +20,24 @@ class Block:
             """
         )
 
+    def client_connected(self, client):
+        parts = client.address[0].rsplit("%", 1)
+        address = ipaddress.ip_address(parts[0])
+        if isinstance(address, ipaddress.IPv6Address):
+            address = address.ipv4_mapped or address
+
+        if address.is_loopback:
+            return
+
+        if ctx.options.block_private and address.is_private:
+            ctx.log.warn(f"Client connection from {client.address[0]} killed by block_private option.")
+            client.error = "Connection killed by block_private."
+
+        if ctx.options.block_global and address.is_global:
+            ctx.log.warn(f"Client connection from {client.address[0]} killed by block_global option.")
+            client.error = "Connection killed by block_global."
+    # FIXME: Remove old proxy core hook below.
+
     def clientconnect(self, layer):
         astr = layer.client_conn.address[0]
 
